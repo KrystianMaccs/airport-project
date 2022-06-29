7393103699c477658fe85842f4317dd9f000313b
Faisal-161 <96625297+Faisal-161@users.noreply.github.com>
2022-06-29 21:10:17 +0600

    Update admin.py

--- flight_booking_website/flight/admin.py
+++ flight_booking_website/flight/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Flight
 
 class FlightAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['aeroplane','departure',
+                   'arrival_time','max_passengers']
+    list_filter = ['aeroplane','destination','arrival_time','max_passengers',]
+
+    search_fields =['departure']                         # this will add filters and search options in the admin page 
 
 admin.site.register(Flight, FlightAdmin)
