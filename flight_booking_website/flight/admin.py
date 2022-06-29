b72d79f5e00fcc899f8e4d7545d771ef18007011
Krystian Maccs <krystianmaccs@gmail.com>
2022-06-29 15:59:45 -0700

    made minor corrections on the admin file

--- flight_booking_website/flight/admin.py
+++ flight_booking_website/flight/admin.py
@@ -3,8 +3,8 @@ from .models import Flight
 
 class FlightAdmin(admin.ModelAdmin):
     list_display = ['aeroplane','departure',
-                   'arrival_time','max_passengers']
-    list_filter = ['aeroplane','destination','arrival_time','max_passengers',]
+                   'arrival_datetime','max_passengers']
+    list_filter = ['aeroplane','destination','arrival_datetime','max_passengers',]
 
     search_fields =['departure']                         # this will add filters and search options in the admin page 
 
