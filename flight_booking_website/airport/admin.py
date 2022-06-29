4d8e3f8cb41fb25a68ee1a8c242a68e7c099e38f
Faisal-161 <96625297+Faisal-161@users.noreply.github.com>
2022-06-29 21:05:18 +0600

    Update admin.py

--- flight_booking_website/airport/admin.py
+++ flight_booking_website/airport/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models import Airport
 
 class AirportAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name','country','code']  #This will add the filters in the admin page
+    list_filter = ['name','country','code']   
+    search_fields = ['country']               # We can search by this in the admin page
 
 admin.site.register(Airport, AirportAdmin)
