107beeaf12302f37643df94505d3ea3a4081792a
Faisal-161 <96625297+Faisal-161@users.noreply.github.com>
2022-06-29 21:08:05 +0600

    Update admin.py

--- flight_booking_website/booking/admin.py
+++ flight_booking_website/booking/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models import Booking
 
 class BookingAdmin(admin.ModelAdmin):
-    pass
+    list_display =[ 'reference_no','passenger_first_name', 'passenger_last_name', 'flight' ,'booking_datetime']
+    list_filter = ['flight','booking_datetime']
+    search_fields = ['reference_no']                # this will add filters and search options in the admin page as mentioned in the excercise3 
 
 admin.site.register(Booking, BookingAdmin)
