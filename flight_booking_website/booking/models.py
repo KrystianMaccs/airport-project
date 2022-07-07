d511e41dc5ca5c8fec7d692eafb305e633521678
Faisal-161 <96625297+Faisal-161@users.noreply.github.com>
2022-07-07 22:34:18 +0600

    Update models.py

--- flight_booking_website/booking/models.py
+++ flight_booking_website/booking/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from Flight.models import Flight
+import random
 
-from flight.models import Flight
+
+def random_string():
+   return str(random.randint(100000, 999999))
+
+
+
+
+
+## invoking the function
 
 class Booking(models.Model):
-    reference_no = models.IntegerField()
+
+
+    reference_no         = models.CharField(max_length=6, default=random_string, unique=True, editable=False )
     passenger_first_name = models.CharField(max_length=20)
-    passenger_last_name = models.CharField(max_length=20)
-    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
-    booking_datetime = models.DateTimeField(auto_now_add=True)
+    passenger_last_name  = models.CharField(max_length=20)
+    flight               = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    booking_datetime     = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.passenger_first_name
