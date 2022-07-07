cbd1fd1d9b6f288ae0c683109ea5fedbf6d694d4
Faisal-161 <96625297+Faisal-161@users.noreply.github.com>
2022-07-07 22:32:34 +0600

    Update views.py

--- flight_booking_website/booking/views.py
+++ flight_booking_website/booking/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Booking
+
+
+def home(request):
+    contex ={
+
+        'Booking':Booking.objects.filter("Booking_datetime")
+    }
+
+    return(request)
+
+
+def Booking_details(request):    #kind a confused here
+    pass
+
 
 # Create your views here.
