5887b07ccfcbbf26d63d106e45bb7edbbe0801c7
Faisal-161 <96625297+Faisal-161@users.noreply.github.com>
2022-07-07 22:39:03 +0600

    Update models.py

--- flight_booking_website/flight/models.py
+++ flight_booking_website/flight/models.py
@@ -1,22 +1,56 @@
 from django.db import models
-from airport.models import Airport
-import datetime
+from Airport.models import Airport
+from datetime import datetime
+
+
+
+def difference(h1, m1,h2, m2):
+
+  t1 =h1 *60 +m1  
+  t2 = h2 * 60 + m2
+
+
 
 class Flight(models.Model):
-    aeroplane = models.CharField(max_length=30)
-    departure = models.ForeignKey(Airport, null=False, related_name="departure_airport", on_delete=models.CASCADE)
-    destination = models.ForeignKey(Airport, null=False, related_name="destination_airport", on_delete=models.CASCADE)
-    departure_datetime = models.DateTimeField('Departure Time', auto_now_add=True)
-    arrival_datetime = models.DateTimeField('Arrival Time', auto_now=True)
-    max_passengers = models.PositiveSmallIntegerField()
-    price = models.PositiveIntegerField()
+
+    aeroplane = models.CharField(max_length=100)
+    departure = models.ForeignKey(Airport, on_delete=models.CASCADE,related_name='departure')
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='destination')
+    departure_datetime = models.DateTimeField(auto_now_add=True)
+    arrival_datetime = models.DateTimeField( max_length=100)  # have to add manually on the database and before doing that make migrations
+    max_passengers = models.PositiveIntegerField()
+    price = models.PositiveSmallIntegerField()
+    
+
 
     def __str__(self):
-        return self.aeroplane
+      return self.aeroplane
 
-    """@property
-    def duration(self):
-        departure_time = datetime.datetime.now()
-        arrival_time = datetime.datetime.now()
-        flight_duration = arrival_time - departure_time    #Not sure about this class may be it will work after the tmeplates are added
-        return flight_duration"""
+
+
+    
+    @property
+    def hourdiff(self):
+
+
+      t1 = h1 * 60 + m1
+      t2 = h2 * 60 + m2
+
+      h1 = self.arrival_datetime.hour
+      m1 = self.arrival_datetime.minute
+      h2 = self.departure_datetime.hour
+      m2 = self.departure_datetime.minute
+
+      if (t1==t2):
+        print("Both are same time")
+      else:
+        diff = t2 - t1
+   
+      h = (int(diff/60)) % 24
+      m = diff % 60
+
+
+      return f"{diff}  hours"
+
+    def __str__(self):
+       return str(self.departure)+ "" + "to" +str(self.destination) 
